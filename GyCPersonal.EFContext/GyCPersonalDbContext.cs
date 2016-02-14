6ab4b74b52b9ad64b6409255c1937dcd753815c2
mupgithub <mup.casa@gmail.com>
2016-02-14 11:58:59 +0100

    Gestion y Control Personal

---
+++ GyCPersonal.EFContext/GyCPersonalDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GyCPersonal.Entidades;
+
+namespace GyCPersonal.EFContext
+{
+    public class GyCPersonalDbContext: DbContext
+    {
+        public GyCPersonalDbContext()
+            : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Producto> Productos { get;set;}
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<DetalleCompra> DetallesCompras { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+
+    }
+}
---
+++ GyCPersonal.Repositorios/RepositorioProductos.cs
@@ -0,0 +1,14 @@
+﻿using GyCPersonal.Entidades;
+using System.Data.Entity;
+
+namespace GyCPersonal.Repositorios
+{
+    public class RepositorioProductos: Repositorio<Producto>        
+    {
+        public RepositorioProductos(DbContext db)
+            :base(db)
+        {
+
+        }
+    }
+}
